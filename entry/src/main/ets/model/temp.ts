5127bdf32d949caece2469d6a726691b9cfe6e83
forcehunter <2578866299@qq.com>
2024-01-03 19:58:11 +0800

    temp: 检验iintention

--- entry/src/main/ets/model/temp.ts
+++ entry/src/main/ets/model/temp.ts
@@ -16,41 +16,41 @@ export type IExpert = {
   userName:string
 }
 
-// export type IFinance = {
-//   bankId:number
-//   combinationIdnum1:string
-//   combinationIdnum2:string
-//   combinationName1:string
-//   combinationName2:string
-//   combinationPhone1:string
-//   combinationPhone2:string
-//   createTime:string
-//   fileInfo:string
-//   financeId:number
-//   idNum:string
-//   money:number
-//   ownName:string
-//   phone:string
-//   rate:number
-//   realName:string
-//   repayment:number
-//   status:number
-//   updateTime:string
-// }
-
-export type IIntention = {
-  address:string
-  amount:number
-  application:string
-  area:number
+export type IFinance = {
+  bankId:number
+  combinationIdnum1:string
+  combinationIdnum2:string
+  combinationName1:string
+  combinationName2:string
+  combinationPhone1:string
+  combinationPhone2:string
   createTime:string
-  item:string
+  fileInfo:string
+  financeId:number
+  idNum:string
+  money:number
+  ownName:string
   phone:string
+  rate:number
   realName:string
-  repaymentPeriod:number
+  repayment:number
+  status:number
   updateTime:string
-  userName:string
 }
+
+// export type IIntention = {
+//   address:string
+//   amount:number
+//   application:string
+//   area:number
+//   createTime:string
+//   item:string
+//   phone:string
+//   realName:string
+//   repaymentPeriod:number
+//   updateTime:string
+//   userName:string
+// }
 
 export type IKnowledge = {
   content:string
