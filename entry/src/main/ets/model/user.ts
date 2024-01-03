d8a2492be9c8020cb93b059350d2b00146c46e32
QYT-WNX <1729566829@qq.com>
2024-01-03 16:51:14 +0800

    feat: 添加用户注册模型

--- entry/src/main/ets/model/user.ts
+++ entry/src/main/ets/model/user.ts
@@ -2,3 +2,10 @@ export type IUserLoginDTO = {
   username: string;
   password: string;
 }
+
+export type IUserRegisterDTO = {
+  userName: string;
+  password: string;
+  nickName: string;
+  avatar: string;
+}
