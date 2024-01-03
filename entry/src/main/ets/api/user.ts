d340ca702fcda369d54108e7e491319647709c0d
QYT-WNX <1729566829@qq.com>
2024-01-03 16:51:23 +0800

    feat: 添加用户注册请求

--- entry/src/main/ets/api/user.ts
+++ entry/src/main/ets/api/user.ts
@@ -1,11 +1,24 @@
 import http from '@ohos.net.http';
 import { AppConfig } from '../config'
-import { IUserLoginDTO } from '../model/user';
+import { IUserLoginDTO, IUserRegisterDTO } from '../model/user';
 
 export const userLoginApi = (data: IUserLoginDTO) => {
   const httpRequest = http.createHttp();
   return httpRequest.request(
     `${AppConfig.baseUrl}/user/login`,
+    {
+      method: http.RequestMethod.POST,
+      extraData: data,
+      header: {
+        'Content-Type': 'application/json'
+      }
+    });
+}
+
+export const userRegisterApi = (data: IUserRegisterDTO) => {
+  const httpRequest = http.createHttp();
+  return httpRequest.request(
+    `${AppConfig.baseUrl}/user/add`,
     {
       method: http.RequestMethod.POST,
       extraData: data,
