04d73b0d8cb51af4f3cf117784675c0d2c10f41e
QYT-WNX <1729566829@qq.com>
2024-01-04 23:25:53 +0800

    feat: 创建地址相关Api

--- entry/src/main/ets/api/address.ts
+++ entry/src/main/ets/api/address.ts
@@ -1,5 +1,6 @@
 import http from '@ohos.net.http';
 import { AppConfig } from '../config'
+import { IAddress } from '../model/address';
 
 export const getAllAddressApi = () => {
   const httpRequest = http.createHttp();
@@ -24,5 +25,46 @@ export const getDefaultAddressApi = () => {
         'Content-Type': 'application/json',
         'Authorization': globalThis.token
       }
+    });
+}
+
+export const deleteAddressApi = (id: number) => {
+  const httpRequest = http.createHttp();
+  return httpRequest.request(
+    encodeURI(`${AppConfig.baseUrl}/address/delete/${id}`),
+    {
+      method: http.RequestMethod.DELETE,
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': globalThis.token
+      }
+    });
+}
+
+export const editAddressApi = (data: IAddress) => {
+  const httpRequest = http.createHttp();
+  return httpRequest.request(
+    encodeURI(`${AppConfig.baseUrl}/address/defaultAddressInfoUpdate`),
+    {
+      method: http.RequestMethod.POST,
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': globalThis.token
+      },
+      extraData: data
+    });
+}
+
+export const addAddressApi = (data: IAddress) => {
+  const httpRequest = http.createHttp();
+  return httpRequest.request(
+    encodeURI(`${AppConfig.baseUrl}/address/add`),
+    {
+      method: http.RequestMethod.POST,
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': globalThis.token
+      },
+      extraData: data
     });
 }
