6556da4de0a2e222794837e887a7260374fb9428
QYT-WNX <1729566829@qq.com>
2024-01-04 08:57:05 +0800

    feat: 添加问题模型

---
+++ entry/src/main/ets/model/question.ts
@@ -0,0 +1,11 @@
+export type IQuestion = {
+  answer: string
+  expertName: string
+  id: number
+  phone: string
+  plantName: string
+  question: string
+  questioner: string
+  status: number
+  title: string
+}
