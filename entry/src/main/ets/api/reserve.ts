ef50a792f36c3d57b263d85b7723a223e97b0f64
QYT-WNX <1729566829@qq.com>
2024-01-04 10:03:48 +0800

    feat: 创建预约Api

---
+++ entry/src/main/ets/api/reserve.ts
@@ -0,0 +1,17 @@
+import http from '@ohos.net.http';
+import { AppConfig } from '../config'
+import { IReserve } from '../model/reserve';
+
+export const addReserveApi = (data: IReserve) => {
+  const httpRequest = http.createHttp();
+  return httpRequest.request(
+    encodeURI(`${AppConfig.baseUrl}/reserve/add`),
+    {
+      method: http.RequestMethod.POST,
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': globalThis.token
+      },
+      extraData: data
+    });
+}
