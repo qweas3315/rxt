5b9541e9eac887135a4daa103361243c7bbceb5e
QYT-WNX <1729566829@qq.com>
2024-01-05 23:48:16 +0800

    feat: 格式化代码

--- entry/src/main/ets/config/index.ts
+++ entry/src/main/ets/config/index.ts
@@ -1,5 +1,5 @@
 export const AppConfig = {
-  baseUrl: 'http://10.202.82.88:9090',
+  baseUrl: 'http://192.168.31.70:9090',
   appName: '融销通',
   hmsKey: 'DAEDAL6hNAI1H+gAHIaXg3nZvgCXdHsA3N7lRuUdraTLrIT1xYpMLYaDFi0DgWlGIN58hLLc7TXhmkZZRGbhpWLYWZraUeb+ArNNAg=='
 }
