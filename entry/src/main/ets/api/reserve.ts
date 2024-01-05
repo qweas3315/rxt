423b87076a2aa9ab0d09f708490974ed723b6a40
QYT-WNX <1729566829@qq.com>
2024-01-05 12:01:51 +0800

    feat: 优化专家预约列表界面

--- entry/src/main/ets/api/reserve.ts
+++ entry/src/main/ets/api/reserve.ts
@@ -28,3 +28,44 @@ export const getUserReserveApi = () => {
       },
     });
 }
+
+export const getExpertReserveApi = () => {
+  const httpRequest = http.createHttp();
+  return httpRequest.request(
+    encodeURI(`${AppConfig.baseUrl}/reserve/selectByKind/expert`),
+    {
+      method: http.RequestMethod.GET,
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': globalThis.token
+      },
+    });
+}
+
+
+export const deleteReserveApi = (id: string) => {
+  const httpRequest = http.createHttp();
+  return httpRequest.request(
+    encodeURI(`${AppConfig.baseUrl}/reserve/delete/${id}`),
+    {
+      method: http.RequestMethod.DELETE,
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': globalThis.token
+      },
+    });
+}
+
+export const updateReserveApi = (data: IReserve) => {
+  const httpRequest = http.createHttp();
+  return httpRequest.request(
+    encodeURI(`${AppConfig.baseUrl}/reserve/update`),
+    {
+      method: http.RequestMethod.PUT,
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': globalThis.token
+      },
+      extraData: data
+    });
+}
