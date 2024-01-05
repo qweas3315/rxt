623e33e965d07edb7dc67fb7862df4e82d04482d
QYT-WNX <1729566829@qq.com>
2024-01-05 21:48:42 +0800

    fix: 修复我的需求和我发布的商品展示BUG

--- entry/src/main/ets/config/index.ts
+++ entry/src/main/ets/config/index.ts
@@ -1,5 +1,5 @@
 export const AppConfig = {
-  baseUrl: 'http://101.201.59.201:9090',
+  baseUrl: 'http://10.202.82.88:9090',
   appName: '融销通',
   hmsKey: 'DAEDAL6hNAI1H+gAHIaXg3nZvgCXdHsA3N7lRuUdraTLrIT1xYpMLYaDFi0DgWlGIN58hLLc7TXhmkZZRGbhpWLYWZraUeb+ArNNAg=='
 }
