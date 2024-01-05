efe7592eea9a0d70b97175afe2b4ad8293cf089e
QYT-WNX <1729566829@qq.com>
2024-01-06 00:15:05 +0800

    feat: 优化数据加载

--- entry/src/main/ets/config/index.ts
+++ entry/src/main/ets/config/index.ts
@@ -1,5 +1,5 @@
 export const AppConfig = {
-  baseUrl: 'http://192.168.31.70:9090',
+  baseUrl: 'http://192.168.225.168:9090',
   appName: '融销通',
   hmsKey: 'DAEDAL6hNAI1H+gAHIaXg3nZvgCXdHsA3N7lRuUdraTLrIT1xYpMLYaDFi0DgWlGIN58hLLc7TXhmkZZRGbhpWLYWZraUeb+ArNNAg=='
 }
