31e9af870739a1b2684b3683d800443177ed228a
QYT-WNX <1729566829@qq.com>
2024-01-05 14:55:24 +0800

    feat: 优化专家角色我的知识界面

--- entry/src/main/ets/api/knowledge.ts
+++ entry/src/main/ets/api/knowledge.ts
@@ -1,5 +1,6 @@
 import http from '@ohos.net.http';
 import { AppConfig } from '../config'
+import { IKnowledge } from '../model/knowledge';
 
 export const getKnowledgePageApi = (current: number) => {
   const httpRequest = http.createHttp();
@@ -10,5 +11,48 @@ export const getKnowledgePageApi = (current: number) => {
       header: {
         'Content-Type': 'application/json'
       }
+    });
+}
+
+export const getExpertKnowledgeApi = () => {
+  const httpRequest = http.createHttp();
+  return httpRequest.request(
+    encodeURI(`${AppConfig.baseUrl}/knowledge/selectByUsername`),
+    {
+      method: http.RequestMethod.GET,
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': globalThis.token
+      }
+    });
+}
+
+export const deleteKnowledgeApi = (id: string) => {
+  const httpRequest = http.createHttp();
+  return httpRequest.request(
+    encodeURI(`${AppConfig.baseUrl}/knowledge/${id}`),
+    {
+      method: http.RequestMethod.DELETE,
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': globalThis.token
+      },
+      extraData: {
+        knowledgeId: id
+      }
+    });
+}
+
+export const updateKnowledgeApi = (data: IKnowledge) => {
+  const httpRequest = http.createHttp();
+  return httpRequest.request(
+    encodeURI(`${AppConfig.baseUrl}/knowledge/${data.knowledgeId}`),
+    {
+      method: http.RequestMethod.PUT,
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': globalThis.token
+      },
+      extraData: data
     });
 }
--- entry/src/main/ets/model/knowledge.ts
+++ entry/src/main/ets/model/knowledge.ts
@@ -1,9 +1,9 @@
 export type IKnowledge = {
-  content: string
-  createTime: string
-  knowledgeId: number
-  ownName: string
-  picPath: string
-  title: string
-  updateTime: string
+  content?: string
+  createTime?: string
+  knowledgeId?: number
+  ownName?: string
+  picPath?: string
+  title?: string
+  updateTime?: string
 }
