ee9ffcfd673b844891a1b0c41517bb27626ef531
QYT-WNX <1729566829@qq.com>
2024-01-05 09:25:24 +0800

    feat: 更新意向模型

---
+++ entry/src/main/ets/model/finance.ts
@@ -0,0 +1,36 @@
+export type IFinance = {
+  bankId?: number
+  combinationIdnum1?: string
+  combinationIdnum2?: string
+  combinationName1?: string
+  combinationName2?: string
+  combinationPhone1?: string
+  combinationPhone2?: string
+  createTime?: string
+  fileInfo?: string
+  financeId?: number
+  idNum?: string
+  money?: number
+  ownName?: string
+  phone?: string
+  rate?: number
+  realName?: string
+  repayment?: number
+  status?: number
+  updateTime?: string
+}
+
+export type IIntention = {
+  address?: string
+  amount?: string
+  application?: string
+  area?: string
+  createTime?: string
+  item?: string
+  phone?: string
+  realName?: string
+  repaymentPeriod?: string
+  updateTime?: string
+  userName?: string
+  avatar?: string
+}
