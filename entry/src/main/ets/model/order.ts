92a691428c214078058ac9976933a88ac145d37e
QYT-WNX <1729566829@qq.com>
2024-01-05 09:25:51 +0800

    feat: 修改订单模型

--- entry/src/main/ets/model/order.ts
+++ entry/src/main/ets/model/order.ts
@@ -28,3 +28,41 @@ export type IShoppingModel = {
   shoppingId: number
   title: string
 }
+
+export type IMyPurchase = {
+  address: string
+  createTime: string
+  ownName: string
+  picture: string
+  purchaseId: number
+  purchaseStatus: number
+  purchaseType: number
+  totalPrice: number
+  updateTime: string
+}
+
+
+export type IPurchaseDetail = {
+  count: number
+  detailId: number
+  orderId: number
+  purchaseId: number
+  sumPrice: number
+  uninPrice: number
+}
+
+
+export type ISellPurchase = {
+  address: string
+  createTime: string
+  orderId: number
+  ownName: string
+  phone: string
+  purchaseId: number
+  purchaseStatus: number
+  purchaseType: number
+  sellPurchaseId: number
+  sumPrice: number
+  uninPricee: number
+  updateTime: number
+}
